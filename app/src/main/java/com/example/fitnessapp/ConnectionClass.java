70e7944492fa1d5e1a2cd7e54dce20688b0b834b
PAUL ABELLANA <paulmabellana@gmail.com>
2024-05-26 04:13:06 +0800

    pictures and gif

--- app/src/main/java/com/example/fitnessapp/ConnectionClass.java
+++ app/src/main/java/com/example/fitnessapp/ConnectionClass.java
@@ -11,12 +11,12 @@
         private static final String TAG = "ConnectionClass";
 
         // For the local database
-        private static final String DATABASE = "fitnessdb"; // local
-        private static final String IP = "10.0.2.2"; // locally hosted in machine
-        private static final String PORT = "3306";
-        private static final String USERNAME = "paul"; // local
-//        private static final String USERNAME = "francis"; // local
-        private static final String PASSWORD = "abellana"; // local
+//        private static final String DATABASE = "fitnessdb"; // local
+//        private static final String IP = "10.0.2.2"; // locally hosted in machine
+//        private static final String PORT = "3306";
+//        private static final String USERNAME = "paul"; // local
+////        private static final String USERNAME = "francis"; // local
+//        private static final String PASSWORD = "abellana"; // local
 //        private static final String PASSWORD = "chavez"; // local
 //        private static final String IP = "192.168.1.4";
 //        private static final String IP = "127.0.0.1";
@@ -24,11 +24,11 @@
 //        private static final String PASSWORD = "123456";
 
         // For the online database
-//        private static final String DATABASE = "sql12709080";
-//        private static final String IP = "sql12.freesqldatabase.com";
-//        private static final String PORT = "3306";
-//        private static final String USERNAME = "sql12709080";
-//        private static final String PASSWORD = "cUFTy1vWpp";
+        private static final String DATABASE = "sql12709080";
+        private static final String IP = "sql12.freesqldatabase.com";
+        private static final String PORT = "3306";
+        private static final String USERNAME = "sql12709080";
+        private static final String PASSWORD = "cUFTy1vWpp";
 
         public static Connection getConnection() {
             Connection conn = null;
